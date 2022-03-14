4b0b65b1863754e9b06a901ea896093aa8275bc2
Rafael Seidi Suzuki Tomita <100170190+RafaelSeidi@users.noreply.github.com>
2022-03-13 23:02:36 -0300

    Add files via upload

---
+++ Randomizacao/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Randomizacao
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = rnd.Next(1, 7);
+
+            label1.Text = x.ToString();
+
+            pictureBox1.Load("dado" + x.ToString() + ".png");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
